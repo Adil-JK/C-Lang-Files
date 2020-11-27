5cd0a1b613aca4f31827252b3b89b52d0a2edf8f
Adil-JK <74587111+Adil-JK@users.noreply.github.com>
2020-11-27 06:30:09 -0800

    Add files via upload

---
+++ 3_LEDS.ino
@@ -0,0 +1,31 @@
+int led0 = 0;
+int led1 = 1;
+int led2 = 2;
+
+bool a;
+bool b;
+bool c;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led0, OUTPUT);
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+pinMode(3, INPUT);
+pinMode(4, INPUT);
+pinMode(5, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+a = digitalRead(3);
+b = digitalRead(4);
+c = digitalRead(5);
+
+digitalWrite(led0, a);
+digitalWrite(led1, b);
+digitalWrite(led2, c);
+
+
+}
---
+++ latch.ino
@@ -0,0 +1,35 @@
+int button0 = 0;
+int button1 = 1;
+int led13 = 13;
+bool state = false;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(button0, INPUT);
+pinMode(button1, INPUT);
+pinMode(led13, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (digitalRead(button0)){
+  state = true;
+}
+
+if (digitalRead(button1)){
+  state = false; 
+}
+
+digitalWrite(led13, state);
+
+//else if ((digitalRead(button1)) {
+//   
+//    state = true;
+// 
+//}
+
+
+
+}
